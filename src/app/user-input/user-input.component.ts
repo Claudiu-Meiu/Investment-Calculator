737f08739d61989136cde55f0e3f027e3299c00a
Claudiu <claudiu-meiu@proton.me>
2024-12-20 19:10:10 +0200

    Added code to calculate investment in app.component.ts; Added form handling in user-input.component.ts and user-input.component.html; Created event emitter to calculate investment when the calculate button is clicked

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.scss'
 })
 export class UserInputComponent {
 
+  @Output() calculate = new EventEmitter<{
+    initialInvestment: number,
+    duration: number, 
+    expectedReturn: number, 
+    annualInvestment: number
+  }>();
+
+  enteredInitialInvestment = '0';
+  enteredAnnualInvestment = '0';
+  enteredExpectedReturn = '5';
+  enteredDurationInYears = '10';
+
+  onSubmit() {
+    this.calculate.emit({
+      initialInvestment: Number(this.enteredInitialInvestment),
+      duration: Number(this.enteredDurationInYears),
+      expectedReturn: Number(this.enteredExpectedReturn),
+      annualInvestment: Number(this.enteredAnnualInvestment)
+    });
+  }
 }
